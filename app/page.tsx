06b11af80f8e99a03595b62ac4f870ed4be37791
Zareef <zmas0005@student.monash.edu>
2024-10-20 18:54:21 +1100

    Add flight search fields

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,15 @@
-import {
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
-import React from "react";
+import { Button, HStack, Input, VStack } from "@chakra-ui/react";
+import FlightTable from "../components/FlightTable";
 
 export default function Page() {
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>From</Th>
-            <Th>To</Th>
-            <Th>Date</Th>
-            <Th>Price</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>somewhere</Td>
-            <Td>somewhere else</Td>
-            <Td>now</Td>
-            <Td>expensive</Td>
-          </Tr>
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <VStack spacing={10}>
+      <HStack>
+        <Input w="min" placeholder="From" />
+        <Input w="min" placeholder="To" />
+        <Button w="min">Find Flights</Button>
+      </HStack>
+      <FlightTable />
+    </VStack>
   );
 }
