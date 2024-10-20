a43486445c78b695bb662aef6e1171c912419d3e
Zareef <zmas0005@student.monash.edu>
2024-10-20 20:57:19 +1100

    Implement first cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// import {onRequest} from "firebase-functions/v2/https";
+import {onRequest} from "firebase-functions/v2/https";
 // import * as logger from "firebase-functions/logger";
 
 // Start writing functions
@@ -17,3 +17,30 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import Amadeus from "amadeus-ts";
+import {defineSecret} from "firebase-functions/params";
+
+const amadeusApiKey = defineSecret("AMADEUS_API_KEY");
+const amadeusApiSecret = defineSecret("AMADEUS_API_SECRET");
+
+export const getFlights = onRequest(
+  {secrets: [amadeusApiKey, amadeusApiSecret]},
+  (request, response) => {
+    const amadeus = new Amadeus({
+      clientId: amadeusApiKey.value(),
+      clientSecret: amadeusApiSecret.value(),
+    });
+
+    amadeus.shopping.flightDestinations
+      .get({
+        origin: "MAD",
+      })
+      .then((apiResponse) => {
+        response.json(apiResponse.result);
+      })
+      .catch((error) => {
+        response.send(error);
+      });
+  }
+);
