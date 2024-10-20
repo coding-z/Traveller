285a3d0d0216b0b320b686232361fa8a5174910e
Zareef <zmas0005@student.monash.edu>
2024-10-20 19:18:52 +1100

    Add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import { Button, HStack, Input, VStack } from "@chakra-ui/react";
+import { Button, Flex, HStack, Input, VStack } from "@chakra-ui/react";
 import FlightTable from "../components/FlightTable";
 
 export default function Page() {
   return (
-    <VStack spacing={10}>
-      <HStack>
-        <Input w="min" placeholder="From" />
-        <Input w="min" placeholder="To" />
-        <Button w="min">Find Flights</Button>
-      </HStack>
+    <VStack w="full" h="full" spacing={100}>
+      <Flex w="full" h={300} bgImage="url('/aircraft-background-image.jpg')" bgSize="cover" bgPosition="center" justifyContent="center" alignItems="center">
+        <HStack bgColor="white" p={50} borderRadius="lg">
+          <Input w="min" placeholder="From" />
+          <Input w="min" placeholder="To" />
+          <Button w="min">Find Flights</Button>
+        </HStack>
+      </Flex>
       <FlightTable />
     </VStack>
   );
--- components/FlightTable.tsx
+++ components/FlightTable.tsx
@@ -1,26 +1,28 @@
-import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Card, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 
 export default function FlightTable() {
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>From</Th>
-            <Th>To</Th>
-            <Th>Date</Th>
-            <Th>Price</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>somewhere</Td>
-            <Td>somewhere else</Td>
-            <Td>now</Td>
-            <Td>expensive</Td>
-          </Tr>
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <Card>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>From</Th>
+              <Th>To</Th>
+              <Th>Date</Th>
+              <Th>Price</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>somewhere</Td>
+              <Td>somewhere else</Td>
+              <Td>now</Td>
+              <Td>expensive</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Card>
   );
 }
