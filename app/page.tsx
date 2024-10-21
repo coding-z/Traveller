332cf8eb2ea96697ae71bbf799894f76302ad285
Zareef <zmas0005@student.monash.edu>
2024-10-21 16:05:55 +1100

    Remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,8 @@
 "use client";
 
-import { Box, Button, Flex, HStack, Input, VStack } from "@chakra-ui/react";
-import FlightTable from "../components/FlightTable";
-import { useState } from "react";
-import { FlightDestinationsResult } from "amadeus-ts";
 import FlightSearcher from "../components/FlightSearcher";
 
-export default function Page() {
-  const [result, setResult] = useState<FlightDestinationsResult>();
-  
-  function handleSearchFlights() {
-    fetch("https://getflights-l2cuob5xda-uc.a.run.app")
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      return result;
-    })
-    .then(setResult)
-    .catch(console.error);
-  }
-  
+export default function Page() {  
   return (
     <FlightSearcher />
     // <FlightTable result={result} />
