be46e028cc7168ec126383a0ad22fd9a18392d61
Zareef <zmas0005@student.monash.edu>
2024-10-22 17:43:41 +1100

    Update function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,7 +18,7 @@ import {onRequest} from "firebase-functions/v2/https";
 //   response.send("Hello from Firebase!");
 // });
 
-// import Amadeus from "amadeus-ts";
+import Amadeus from "amadeus-ts";
 import {defineSecret} from "firebase-functions/params";
 
 const amadeusApiKey = defineSecret("AMADEUS_API_KEY");
@@ -27,7 +27,6 @@ const amadeusApiSecret = defineSecret("AMADEUS_API_SECRET");
 export const getCheapestFlights = onRequest(
   {cors: true, secrets: [amadeusApiKey, amadeusApiSecret]},
   (request, response) => {
-    response.status(200).send("Function working");
     // if (
     //   request.query.origin === undefined ||
     //   request.query.destination === undefined
@@ -36,22 +35,22 @@ export const getCheapestFlights = onRequest(
     //     .status(400)
     //     .send("Must supply origin and destination parameters");
     // } else {
-    //   const amadeus = new Amadeus({
-    //     clientId: amadeusApiKey.value(),
-    //     clientSecret: amadeusApiSecret.value(),
-    //   });
+    const amadeus = new Amadeus({
+      clientId: amadeusApiKey.value(),
+      clientSecret: amadeusApiSecret.value(),
+    });
 
-    //   amadeus.shopping.flightDates
-    //     .get({
-    //       origin: request.query.origin.toString(),
-    //       destination: request.query.destination.toString(),
-    //     })
-    //     .then((apiResponse) => {
-    //       response.status(200).json(apiResponse.result);
-    //     })
-    //     .catch((error) => {
-    //       response.status(500).send(error.code);
-    //     });
+    amadeus.shopping.flightDestinations
+      .get({
+        origin: request.query.origin?.toString() ?? "",
+        // destination: request.query.destination?.toString() ?? "",
+      })
+      .then((apiResponse) => {
+        response.status(200).json(apiResponse);
+      })
+      .catch((error) => {
+        response.status(500).json(error);
+      });
     // }
   }
 );
